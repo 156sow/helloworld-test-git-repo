c53b7a3f11cdee70f001a565d4413d42fdb5ff9f
156sow <sowmyasep123@gmail.com>
2022-03-21 03:24:39 -0700

    web server program

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def homePage():
-    return('Hello World')
+    return('Hello world')
 
 if(__name__=="__main__"):
-    app.run()    
+    app.run()     
